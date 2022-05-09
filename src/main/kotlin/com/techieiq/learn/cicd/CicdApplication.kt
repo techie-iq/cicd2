85e9665f8a0b550fa8e0459bec77c90c63dd4552
Ali Yusha <ali.yusha@hotmail.com>
2022-05-09 12:52:34 +0200

    init

--- src/main/kotlin/com/techieiq/learn/cicd/CicdApplication.kt
+++ src/main/kotlin/com/techieiq/learn/cicd/CicdApplication.kt
@@ -1,13 +1,33 @@
 package com.techieiq.learn.cicd
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @SpringBootApplication
 class CicdApplication
-
+//http://cicd-one.staging-system.svc.cluster.local:8080
+var client = WebClient.create("http://cicd-one.staging-system.svc.cluster.local:8080")
 fun main(args: Array<String>) {
     runApplication<CicdApplication>(*args)
+}
+
+@RestController
+@EnableScheduling
+class Controller {
+
+    @Scheduled(fixedDelay = 2000)
+   fun getColor() {
+        println(callColor().block().toString())
+    }
+}
+
+fun callColor(): Mono<String> {
+    return client.get().uri("/api/color").retrieve().bodyToMono(String::class.java)
 }
